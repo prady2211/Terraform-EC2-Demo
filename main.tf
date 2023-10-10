dc72f302a03098d9ee952bb90386b946a5d7e8af
prady2211 <prade06@gmail.com>
2023-10-10 08:43:13 +0000

    main.tf modified

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 resource "aws_key_pair" "key"{
  key_name = "keypair"
- public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDI+VRr67cynVngSAk594H/Z4SeOZ5EPq00rba+0TD+iA98/0Vnnlx9COTQdVoxfK9zRc6ME6KLB50zwtjffObRgDMxkle9c8gNpjIfRFogb8P9IUEVMJ34XJZSmMrhq58Vrq36iIfs5miYxp3GZYkQakFfs74EbAWeJIdkRhZ+xOZCTJISalV5zOVEge4ijXUvyzlssZf6NsUnlfblZAeY2GxBqN+Mt7+zAczgEWOPY+fV8FToOl30pU8ECJlJvJBcKYx2HU3RZXobLap5NFg+Tlo1duZAk1U9w6Jw0zuwrNGEBYC+4EQTGzTtRPrnFfr1B7ygwHl8zAxFdXmshMGaiuEhg60WDr7goe7OT68ADJe7KE0EYBpLzsVUgB62HP+JvINyEKECjbx+jgVsi5fnJ+iVvdFohFteyF+sJfrpc3A+1VuWKfNgWjFvbyglw7cJmDmsDMN43a39vD0bvNt3pTbLOYRK9tMheV8ov4VJk+yXXgCBG6Yw4z3tRvZAg3c= ubuntu@ip-172-31-14-145"
+ public_key = " "
 
 }
 
